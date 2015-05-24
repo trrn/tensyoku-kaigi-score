ca48a1eea9769cc87969901bf0d909bcb407408c
gli <gli@mbp.local>
2015-05-24 13:33:34 +0900

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -7,13 +7,13 @@ function patch() {
     var em_score, user_element = 0, score_element = 0;
     jQuery(this).find("tr").each(function() {
       var th = jQuery(this).find("th");
-      if (th) {
+      if (th.length) {
         var em = th.find("em");
         if (em.length) { // total score for graph
           em_score = em;
-        } else if (th.text().match(/^\d.0$/)) { // score
+        } else if (th.text().match(/^\d.0$/)) { // each score
           var score = th.text()|0;
-          var user = (jQuery(this).find(".value,.last").text().match(/\((\d+)\)/)||[])[1]|0;
+          var user = (jQuery(this).find(".value, .last").text().match(/\((\d+)\)/)||[])[1]|0;
           if (user) {
             score_element += score * user;
             user_element += user;
@@ -22,9 +22,7 @@ function patch() {
       }
     });
     var score_honest = (score_element/user_element).toFixed(1);
-    if (isNaN(score_honest)) {
-      score_honest = "0.0";
-    }
+    if (isNaN(score_honest)) score_honest = 0.0.toFixed(1);
     em_score && em_score.html(
       score_honest + ' <span class="tensyoku-score">(' + em_score.text() + ')</span>');
 
@@ -41,6 +39,7 @@ function patch() {
 
   // total score
   var score_honest_total = (score_total/user_total).toFixed(1);
+  if (isNaN(score_honest_total)) score_honest_total = 0.0.toFixed(1);
   jQuery(".point").each(function() {
     jQuery(this).find("em").each(function() {
       jQuery(this).html(
@@ -50,13 +49,10 @@ function patch() {
 
   // star all
   jQuery("p[class^='star01_'], span[class^='star01_'], li.point").each(function() {
-    var score = (jQuery(this).find("em").text().match(/(\d.\d)/)||[])[0];
-    if (score) {
-      var klass = 'star01_' + (Math.round(parseFloat(score)*2)/2)*10;
-      jQuery(this).removeClass(function(index, css) {
-        return (css.match(/star01_\S+/g)||[])[0];
-      }).addClass(klass);
-    }
+    var score = parseFloat((jQuery(this).find("em").text().match(/(\d.\d)/)||[])[0]).toFixed(1);
+    jQuery(this).removeClass(function(index, css) {
+      return (css.match(/star01_\S+/)||[])[0];
+    }).addClass('star01_' + (((Math.round(score*2)/2)*10)||"00"));
   });
 }
 
